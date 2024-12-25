f184cd509a4f643f60eb42c77e2eccc4ad1407aa
markadc <markadc@126.com>
2024-12-25 18:46:51 +0800

    '新增开播关播方法'

--- meituan.py
+++ meituan.py
@@ -21,6 +21,18 @@ class MeituanSpider:
     msg_queue = Queue()
     unq_msgs = []
 
+    def close_live(self, cookie, live_id):
+        url = f"https://mlive.meituan.com/live/centercontrol/component/liveadmin/stoplive?liveid={live_id}&appid=10&yodaReady=h5&csecplatform=4&csecversion=2.4.0"
+        headers = {"User-Agent": ua, "Cookie": cookie}
+        res = requests.get(url, headers=headers)
+        print(res.text)
+
+    def start_live(self, cookie, live_id):
+        url = f"https://mlive.meituan.com/live/centercontrol/component/liveadmin/startlive?liveid={live_id}&appid=10&yodaReady=h5&csecplatform=4&csecversion=2.4.0"
+        headers = {"User-Agent": ua, "Cookie": cookie}
+        res = requests.get(url, headers=headers)
+        print(res.text)
+
     def get_liveid(self, short_url: str) -> str:
         resp = session.get(short_url, allow_redirects=False)
         new_url = resp.headers["Location"]
@@ -124,18 +136,21 @@ def test2():
 
 def test3():
     """测试，弹商品卡"""
-    cookie = "uuid=90e82c208aae4c66b980.1731548694.1.0.0; _lxsdk_cuid=19328584150c8-0904ee868b3752-4c657b58-1fa400-19328584150c8; WEBDFPID=6w9zu3z2u8z35uwwz04745z319w8x4vx8067141vvu297958446wu7zu-2046908734983-1731548734548SOMGSWMfd79fef3d01d5e9aadc18ccd4d0c95071170; _ga=GA1.1.1078190637.1732238498; _ga_FSX5S86483=GS1.1.1732513784.2.0.1732513784.0.0.0; _ga_LYVVHCWVNG=GS1.1.1732513784.2.0.1732513784.0.0.0; iuuid=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lxsdk=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; __asource=pc; __UTYPE_3=3; lt=AgHTJR1IkrYB6kEp_meviJ93iVPbpMeB0WNeR7JP6Xbwi65ZpyUyunWXmD4owYjQs6_LEDtc9-oc2QAAAABrJQAAjtMC2-sOu2NCBGd80y_iIg6np6ezaMO6gqXwNXXW6o9fQflmhtlJULXOubhN7IK0; u=1843768220; n=Hjiah123; mlive_anchor_token=2mliveanchorAgHTJR1IkrYB6kEp_meviJ93iVPbpMeB0WNeR7JP6Xbwi65ZpyUyunWXmD4owYjQs6_LEDtc9-oc2QAAAABrJQAAjtMC2-sOu2NCBGd80y_iIg6np6ezaMO6gqXwNXXW6o9fQflmhtlJULXOubhN7IK0; __UTYPE_2=2; edper=AgHTJR1IkrYB6kEp_meviJ93iVPbpMeB0WNeR7JP6Xbwi65ZpyUyunWXmD4owYjQs6_LEDtc9-oc2QAAAABrJQAAjtMC2-sOu2NCBGd80y_iIg6np6ezaMO6gqXwNXXW6o9fQflmhtlJULXOubhN7IK0; token=AgHTJR1IkrYB6kEp_meviJ93iVPbpMeB0WNeR7JP6Xbwi65ZpyUyunWXmD4owYjQs6_LEDtc9-oc2QAAAABrJQAAjtMC2-sOu2NCBGd80y_iIg6np6ezaMO6gqXwNXXW6o9fQflmhtlJULXOubhN7IK0; pragma-newtoken=AgHTJR1IkrYB6kEp_meviJ93iVPbpMeB0WNeR7JP6Xbwi65ZpyUyunWXmD4owYjQs6_LEDtc9-oc2QAAAABrJQAAjtMC2-sOu2NCBGd80y_iIg6np6ezaMO6gqXwNXXW6o9fQflmhtlJULXOubhN7IK0; _lxsdk_s=193f158fa1d-4dd-160-65%7C%7C273"
-    live_id = "9393594"
+    cookie = "uuid=90e82c208aae4c66b980.1731548694.1.0.0; _lxsdk_cuid=19328584150c8-0904ee868b3752-4c657b58-1fa400-19328584150c8; WEBDFPID=6w9zu3z2u8z35uwwz04745z319w8x4vx8067141vvu297958446wu7zu-2046908734983-1731548734548SOMGSWMfd79fef3d01d5e9aadc18ccd4d0c95071170; _ga=GA1.1.1078190637.1732238498; _ga_FSX5S86483=GS1.1.1732513784.2.0.1732513784.0.0.0; _ga_LYVVHCWVNG=GS1.1.1732513784.2.0.1732513784.0.0.0; iuuid=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lxsdk=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; __asource=pc; __UTYPE_3=3; __UTYPE_2=2; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; mtcdn=K; lt=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; u=3005615897; n=%E6%A9%99%E7%95%99%E9%A6%99aaa; mlive_anchor_token=2mliveanchorAgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; edper=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; token=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; pragma-newtoken=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; _lxsdk_s=193fc982674-603-2ac-83d%7C%7C52"
+    live_id = "9423883"
     # goods_id = "885732936"  # 80 代 100 元代金券
-    goods_id = "970184788"  # 60分钟足疗
+    # goods_id = "970184788"  # 60分钟足疗
     # goods_id = "1192575776"  # 【疲劳解压】｜60分钟全身精油SPA
-    # goods_id = "1171387952"  # 【解压放松】精油开背｜刮痧
+    goods_id = "1171387952"  # 【解压放松】精油开背｜刮痧
     m = MeituanSpider()
     # m.explain_goods(cookie, live_id, goods_id, 9)
-    m.explain_goods(cookie, live_id, goods_id, 13)
+    m.explain_goods(cookie, live_id, "1209348083", 9)
 
 
 if __name__ == "__main__":
     # test()
     # test2()
-    test3()
+    # test3()
+    cookie = "uuid=90e82c208aae4c66b980.1731548694.1.0.0; _lxsdk_cuid=19328584150c8-0904ee868b3752-4c657b58-1fa400-19328584150c8; WEBDFPID=6w9zu3z2u8z35uwwz04745z319w8x4vx8067141vvu297958446wu7zu-2046908734983-1731548734548SOMGSWMfd79fef3d01d5e9aadc18ccd4d0c95071170; _ga=GA1.1.1078190637.1732238498; _ga_FSX5S86483=GS1.1.1732513784.2.0.1732513784.0.0.0; _ga_LYVVHCWVNG=GS1.1.1732513784.2.0.1732513784.0.0.0; iuuid=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lxsdk=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; __asource=pc; __UTYPE_3=3; __UTYPE_2=2; mtcdn=K; lt=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; u=3005615897; n=%E6%A9%99%E7%95%99%E9%A6%99aaa; mlive_anchor_token=2mliveanchorAgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; edper=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; token=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; pragma-newtoken=AgFjIIzaiKoyBCEG7-iZ_uFLnM76roF_MKYO_3B7u9OE5jy9f8Pg4G0p9yX2Zr9fLettL1PAEGxbzQAAAABrJQAAGQeCD3n3x6juH1svRl_GcbR_xqvSDRERi2Uve14Dab98S4471YqbFvyyP1c8AdO8; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; _lxsdk_s=193fce62be1-3a9-f6e-8ab%7C%7C86"
+    live_id = "9426028"
+    MeituanSpider().start_live(cookie, live_id)
