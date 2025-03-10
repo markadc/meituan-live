972128e620bc5ef3b9ac9c040da2b92b3f18408b
markadc <markadc@126.com>
2025-03-10 18:09:04 +0800

    ...

--- goods_spider.py
+++ goods_spider.py
@@ -1,11 +1,38 @@
 import json
 import time
+from dataclasses import dataclass
+from typing import Tuple, List
 
 import requests
 from loguru import logger as log
+from rich.pretty import pprint
 
 
-class MeituanGoodsSpider:
+@dataclass
+class Goods:
+    name: str
+    id: str
+    type_id: str
+    market_price: str
+    discount: str
+    sale_price: str
+    stock: str
+    cover: str
+
+    def tozh(self):
+        zh = {
+            "商品名称": self.name,
+            "商品ID": self.id,
+            "原价": self.market_price,
+            "折扣": self.discount,
+            "现价": self.sale_price,
+            "库存": self.stock,
+            "封面图": self.cover
+        }
+        return zh
+
+
+class MT_GoodsSpider:
     def __init__(self, cookie: str):
         self.cookie = cookie
         self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
@@ -33,10 +60,11 @@ class MeituanGoodsSpider:
         url = "https://i.meituan.com/mapi/dzu/live/goods/goodslisttob.bin"
         resp = requests.post(url, headers=headers, params=params, json=data)
         jsonStr = resp.text
-        print("{}（直播间id）的商品信息如下".format(live_id))
         jsonData = json.loads(jsonStr)
         assert jsonData["code"] == 0, jsonStr
-        ggs = []
+
+        print("{}（美团直播间ID）的商品信息如下".format(live_id))
+        gtids = []
         for one in jsonData["data"]["goodsList"]:
             goods_id = one["goodsId"]
             goods_type_id = one["goodsType"]
@@ -44,12 +72,12 @@ class MeituanGoodsSpider:
             market_price = one["goodsPriceInfo"]["marketPrice"]
             discount = one["goodsPriceInfo"]["discount"]
             sale_price = one["goodsPriceInfo"]["salePrice"]
-            stock = one["reductionActivityInfo"]["remainStock"] if one["reductionActivityInfo"] else None
+            stock = one["reductionActivityInfo"]["remainStock"] if one["reductionActivityInfo"] else "不限"
             img_url = one["picUrl"]
-            meta = goods_id, goods_type_id, title, market_price, discount, sale_price, stock, img_url
-            ggs.append((goods_id, goods_type_id))
-            print(meta)
-        return ggs
+            goods = Goods(name=title, id=goods_id, type_id=goods_type_id, market_price=market_price, discount=discount, sale_price=sale_price, stock=stock, cover=img_url)
+            pprint(goods.tozh())
+            gtids.append((goods_id, goods_type_id))
+        return gtids
 
     def explain_goods(self, live_id, goods_id, goods_type_id):
         """弹商品卡"""
@@ -66,9 +94,8 @@ class MeituanGoodsSpider:
         res = requests.get(url, params, headers=self.headers)
         log.info(res.text)
 
-    def add_goods(self, live_id, goods_id, goods_type_id):
-        """添加直播商品"""
-        goods_id, goods_type_id = str(goods_id), str(goods_type_id)
+    def batch_add_goods(self, live_id, gtids: List[Tuple[int | str, int | str]]):
+        """批量添加直播商品"""
         api = "https://mlive.meituan.com/live/centercontrol/component/goods/goodsbatchaddendpoint"
         params = {
             "appid": "10",
@@ -80,15 +107,11 @@ class MeituanGoodsSpider:
         data = {
             "liveid": live_id,
             "hidden": False,
-            "goodsList": [
-                {
-                    "bizId": goods_id,
-                    "bizType": goods_type_id,
-                }
-            ],
+            "goodsList": [{"bizId": v[0], "bizType": v[1]} for v in gtids],
             "needResultExcel": False,
             "appid": 10
         }
+        log.debug("正在添加 {} 个商品".format(len(gtids)))
         res = requests.post(api, params=params, headers=self.headers, json=data)
         log.info(res.text)
 
@@ -105,20 +128,62 @@ class MeituanGoodsSpider:
             "csecplatform": "4",
             "csecversion": "3.1.0"
         }
+        log.debug("正在删除 1 个商品")
         res = requests.get(url, params, headers=self.headers)
         log.info(res.text)
 
-    def sort_goods(self):
+    def batch_delete_goods(self, live_id, gtids: List[Tuple[str | int, str | int]]):
+        """批量删除商品"""
+        api = "https://mlive.meituan.com/live/centercontrol/component/goods/goodsbatchdeleteendpoint"
+        params = {
+            "appid": "10",
+            "liveid": live_id,
+            "yodaReady": "h5",
+            "csecplatform": "4",
+            "csecversion": "3.1.0"
+        }
+        data = {
+            "liveid": live_id,
+            "goodsList": [{"bizId": gtid[0], "bizType": gtid[1]} for gtid in gtids],
+            "appid": 10
+        }
+        log.debug("正在删除 {} 个商品".format(len(gtids)))
+        res = requests.post(api, params=params, headers=self.headers, json=data)
+        log.info(res.text)
+
+    def sort_goods(self, livd_id, gtids: list):
         """排序直播商品"""
+        api = "https://mlive.meituan.com/live/centercontrol/component/goods/goodssortendpoint"
+        params = {
+            "appid": "10",
+            "liveid": livd_id,
+            "yodaReady": "h5",
+            "csecplatform": "4",
+            "csecversion": "3.1.0"
+        }
+        data = {
+            "liveid": 10023596,
+            "goodsList": [{"bizId": v[0], "bizType": v[1], "rank": i + 1, "rankChange": 1} for i, v in enumerate(gtids)],
+            "isPagingSort": True,
+            "appid": 10
+        }
+        log.debug("正在排序商品")
+        res = requests.post(api, params=params, headers=self.headers, json=data)
+        log.info(res.text)
 
 
 if __name__ == '__main__':
-    cookie = "uuid=90e82c208aae4c66b980.1731548694.1.0.0; _lxsdk_cuid=19328584150c8-0904ee868b3752-4c657b58-1fa400-19328584150c8; _ga=GA1.1.1078190637.1732238498; _ga_FSX5S86483=GS1.1.1732513784.2.0.1732513784.0.0.0; _ga_LYVVHCWVNG=GS1.1.1732513784.2.0.1732513784.0.0.0; iuuid=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lxsdk=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; lt=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; u=3005615897; n=%E6%A9%99%E7%95%99%E9%A6%99aaa; mlive_anchor_token=2mliveanchorAgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; __asource=pc; __UTYPE_2=2; edper=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; token=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; pragma-newtoken=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; WEBDFPID=6w9zu3z2u8z35uwwz04745z319w8x4vx8067141vvu297958446wu7zu-1739927235146-1736302758502COKEQAYfd79fef3d01d5e9aadc18ccd4d0c95071380; _lxsdk_s=1951830902e-9f9-f02-81d%7C%7C29"
-    meituan = MeituanGoodsSpider(cookie)
-    lid = 10039738
-    gid = 1244770138
-    tid = 9
-    meituan.crawl_goods(lid)
-    # meituan.add_goods(lid, gid, tid)
+    cookie = "uuid=90e82c208aae4c66b980.1731548694.1.0.0; _lxsdk_cuid=19328584150c8-0904ee868b3752-4c657b58-1fa400-19328584150c8; _ga=GA1.1.1078190637.1732238498; _ga_FSX5S86483=GS1.1.1732513784.2.0.1732513784.0.0.0; _ga_LYVVHCWVNG=GS1.1.1732513784.2.0.1732513784.0.0.0; iuuid=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; _lxsdk=8CBE200AAAE11F3C9FC1305E8037CC1DBB492C39DEA4EAE4F593908FF2BEE706; lt=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; u=3005615897; n=%E6%A9%99%E7%95%99%E9%A6%99aaa; mlive_anchor_token=2mliveanchorAgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; __asource=pc; __UTYPE_2=2; edper=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; token=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; pragma-newtoken=AgHbIEj86vnFq2i24xlwEclTj0n5IfQSpvYWRwQXytQA5yYEtbOBmRqy-MP5oRlo7iLcLv6brStarAAAAAAxJgAAPhLAzH0sKAz6ZjO8DD5pXeKxpz4WC4Yt7PyRTCROe-BZ_NIpGiGvqhM-21hQTlvf; WEBDFPID=6w9zu3z2u8z35uwwz04745z319w8x4vx8067141vvu297958446wu7zu-1741655272925-1736302758502COKEQAYfd79fef3d01d5e9aadc18ccd4d0c95071380; _lxsdk_s=1957f72da12-b11-b6d-b3b%7C%7C47"
+    meituan = MT_GoodsSpider(cookie)
+    # lid = 10279311
+    # gid = 1244770138
+    # tid = 9
+    # meituan.crawl_goods(10139816)
     # meituan.delete_goods(lid, gid, tid)
     # meituan.explain_goods(lid, gid, tid)
+
+    # lid = 10045583
+    # gtids = [(915819059, 9), (1127235825, 9), (1244770138, 9)]
+    # meituan.batch_add_goods(lid, gtids)
+    # meituan.batch_delete_goods(lid, gtids)
+    # meituan.sort_goods(lid, gtids)
--- std.py
+++ std.py
@@ -1,5 +1,3 @@
-import datetime
-import json
 import threading
 import time
 from abc import ABC, abstractmethod
@@ -12,17 +10,47 @@ log = Log()
 class StandardBarrageSpider(ABC):
     """标准弹幕爬虫"""
 
-    def __init__(self, barrs=None, websocket=None):
-        self.websocket = websocket
-        self.barrs = barrs
+    def __init__(self, websocket=None):
+        self.ws = websocket
+        self.cached = []
+        self.last_barrage_time = time.time()
+        self.barrage_interval = 3
+
+    async def process_message(self, ws, message: dict):
+        """处理弹幕爬虫的消息"""
+        if not message:
+            await asyncio.sleep(0.5)
+            return
+
+        # 无脑发送即可
+        if time.time() - self.last_barrage_time > self.barrage_interval:
+            await self.send_to_slzb(ws, message)
+            return
+
+        # 需要进行去重
+        if message not in self.cached:
+            await self.send_to_slzb(ws, message)
+
+    async def send_to_slzb(self, ws, one: dict):
+        """给 SLZB 发送弹幕消息"""
+        # await ws.send(json.dumps(one))
+        log.success("已向 SLZB 发送消息 => {}".format(one))
+        await asyncio.sleep(0.1)
+
+        # 加入到全局弹幕
+        self.cached.append(one)
+        self.cached = self.cached[100:] if len(self.cached) > 200 else self.cached
+
+        # 更新弹幕时间
+        self.last_barrage_time = time.time()
 
     @abstractmethod
     def listen(self):
-        """从美团不停的监听弹幕"""
+        """不停的监听弹幕"""
         pass
 
     @abstractmethod
-    def pull_msg(self):
+    def pull(self):
         """从队列里取一个弹幕"""
         pass
 
@@ -31,40 +59,27 @@ class StandardBarrageSpider(ABC):
         t.daemon = True
         t.start()
         while True:
-            spider_msg = self.pull_msg()
-            if not spider_msg:
-                await asyncio.sleep(0.2)
-                continue
-            log.success(f"从队列取出  ==>  {spider_msg}")
-            if not self.barrs:
-                continue
-            self.barrs = self.barrs[100:] if len(self.barrs) > 200 else self.barrs
-            if spider_msg not in self.barrs:
-                self.barrs.append(spider_msg)
-                if not self.websocket:
-                    continue
-                await self.websocket.send(json.dumps(spider_msg))
-                print(f"{datetime.datetime.now()}\t已向前端发送消息\n{spider_msg}\n")
-                await asyncio.sleep(0.2)
+            msg = self.pull()
+            await self.process_message(self.ws, msg)
 
 
 import queue
 import random
 
 
-class Test(StandardBarrageSpider):
+class Test_STD_Spider(StandardBarrageSpider):
     q = queue.Queue()
 
     def listen(self):
         while True:
-            v = random.randint(1, 10000)
-            log.info(f"{v} <= 生产")
+            v = random.randint(1, 5)
+            log.info(f"生产 => {v}")
             self.q.put(v)
             time.sleep(random.randint(1, 3))
 
-    def pull_msg(self):
+    def pull(self):
         try:
-            msg = self.q.get(timeout=1)
+            msg = self.q.get(timeout=2)
             return msg
         except queue.Empty:
             log.warning("超时")
@@ -73,5 +88,5 @@ class Test(StandardBarrageSpider):
 if __name__ == "__main__":
     import asyncio
 
-    t = Test()
-    asyncio.run(t.start())
+    tss = Test_STD_Spider()
+    asyncio.run(tss.start())
